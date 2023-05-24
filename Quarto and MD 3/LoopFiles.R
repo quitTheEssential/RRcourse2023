d7f019f2ba23b38ec62d24a74a380a91e3292377
Robert Kowalczyk <robertkowalczyk011@gmail.com>
2023-05-24 10:15:25 +0200

    Added file to loop through the Assingment for Quarto 3

--- Quarto and MD 3/LoopFiles.R
+++ Quarto and MD 3/LoopFiles.R
@@ -1,8 +1,9 @@
 # Rendering
 library(quarto)
+setwd('C:/Users/robso/Projects/Reproducible Research/RRcourse2023/Quarto and MD 3')
 for (i in 1:8){
 quarto_render("Assignment.qmd", execute_params = list(
   season = i
-))
+), output_file = paste0("Report-",i,".html"))
   
 }
